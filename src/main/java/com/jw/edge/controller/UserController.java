446efda0ee58b17413ba2a901638b775406af8ca
dy <387768965@qq.com>
2019-06-05 14:44:35 +0800

    init

--- src/main/java/com/jw/edge/controller/UserController.java
+++ src/main/java/com/jw/edge/controller/UserController.java
@@ -6,7 +6,7 @@ import com.jw.edge.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+@RequestMapping("/api")
 @RestController
 public class UserController {
 
