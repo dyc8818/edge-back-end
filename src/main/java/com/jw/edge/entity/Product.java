3922813325adb7b3436e7c981ce72148553e4834
dy <387768965@qq.com>
2019-06-14 09:31:04 +0800

    修复产品创建和刷新

--- src/main/java/com/jw/edge/entity/Product.java
+++ src/main/java/com/jw/edge/entity/Product.java
@@ -1,5 +1,6 @@
 package com.jw.edge.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,6 +16,7 @@ public class Product {
     private Boolean productAccessForGateway;
     private String productConnectWay;
     private String productDataFormat;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date productCreateTime;
 
     public Date getProductCreateTime() {
