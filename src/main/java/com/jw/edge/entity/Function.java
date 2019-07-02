554e61ba991cf8605be2e818cda145a1582bf573
liyl <liyl2yy@gmail.com>
2019-07-02 21:02:20 +0800

    gongneng

--- src/main/java/com/jw/edge/entity/Function.java
+++ src/main/java/com/jw/edge/entity/Function.java
@@ -14,6 +14,90 @@ public class Function {
     private String functionName;
     private String functionIdentifier;
     private String functionDesc;
+    private String holdingRegister;
+    private String valueType;
+    private String readWrite;
+    private String valueSize;
+    private String valueScale;
+    private String valueMinimum;
+    private String valueMaximum;
+    private String defaultValue;
+    private String unitsType;
+    private String defaultUnits;
+
+    public String getValueType() {
+        return valueType;
+    }
+
+    public void setValueType(String valueType) {
+        this.valueType = valueType;
+    }
+
+    public String getReadWrite() {
+        return readWrite;
+    }
+
+    public void setReadWrite(String readWrite) {
+        this.readWrite = readWrite;
+    }
+
+    public String getValueSize() {
+        return valueSize;
+    }
+
+    public void setValueSize(String valueSize) {
+        this.valueSize = valueSize;
+    }
+
+    public String getValueScale() {
+        return valueScale;
+    }
+
+    public void setValueScale(String valueScale) {
+        this.valueScale = valueScale;
+    }
+
+    public String getValueMinimum() {
+        return valueMinimum;
+    }
+
+    public void setValueMinimum(String valueMinimum) {
+        this.valueMinimum = valueMinimum;
+    }
+
+    public String getValueMaximum() {
+        return valueMaximum;
+    }
+
+    public void setValueMaximum(String valueMaximum) {
+        this.valueMaximum = valueMaximum;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getUnitsType() {
+        return unitsType;
+    }
+
+    public void setUnitsType(String unitsType) {
+        this.unitsType = unitsType;
+    }
+
+    public String getDefaultUnits() {
+        return defaultUnits;
+    }
+
+    public void setDefaultUnits(String defaultUnits) {
+        this.defaultUnits = defaultUnits;
+    }
+
+
 
 
     /*public Function() {
@@ -48,12 +132,12 @@ public class Function {
         this.functionName = functionName;
     }
 
-    public String getFunctionIdentifier() {
-        return functionIdentifier;
+    public String getHoldingRegister() {
+        return holdingRegister;
     }
 
-    public void setFunctionIdentifier(String functionIdentifier) {
-        this.functionIdentifier = functionIdentifier;
+    public void setHoldingRegister(String holdingRegister) {
+        this.holdingRegister = holdingRegister;
     }
 
     public String getFunctionDesc() {
