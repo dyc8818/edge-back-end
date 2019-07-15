b74f68bc5df2f990e7c765910e39333c18944acf
danielxsh <14211024@bjtu.edu.cn>
2019-07-15 21:30:08 +0800

    规则引擎修复

--- src/main/java/com/jw/edge/controller/api/RuleController.java
+++ src/main/java/com/jw/edge/controller/api/RuleController.java
@@ -1,9 +1,12 @@
 package com.jw.edge.controller.api;
 
 import com.alibaba.fastjson.JSONObject;
+import com.jw.edge.dao.DeviceRepository;
 import com.jw.edge.entity.Function;
 import com.jw.edge.entity.Rule;
+import com.jw.edge.entity.Device;
 import com.jw.edge.service.RuleService;
+import com.jw.edge.service.DeviceService;
 import com.jw.edge.service.FunctionService;
 import com.jw.edge.util.LayuiTableResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,10 @@ public class RuleController {
     RuleService ruleService;
     @Autowired
     FunctionService functionService;
+    @Autowired
+    DeviceService deviceService;
+    @Autowired
+    DeviceRepository deviceRepository;
 
     //设备功能映射
     @GetMapping("/ruleParas")
@@ -31,23 +38,40 @@ public class RuleController {
         return functions;
     }
 
+    //对应设备映射
+    @GetMapping("/ruleToDevices")
+    @ResponseBody
+    public List<Device> getruleToDevices() {
+        List<Device> devices =  deviceService.findAllDevice();
+        // LayuiTableResultUtil<List<Product>> productsTable=new LayuiTableResultUtil<List<Product>>();
+        return devices;
+    }
+
     //控制设备激活
     @PutMapping("/changeRuleStatus")
     @ResponseBody
-    public int changeStatus(@RequestBody JSONObject ruleID){
+    public int changeRuleStatus(@RequestBody JSONObject ruleID){
         JSONObject obj = new JSONObject(ruleID);//将json字符串转换为json对象
 
         System.out.println("ruleid:"+ruleID);
         System.out.println("ruleid:"+ruleID.getString("ruleId"));
-        Rule rule = ruleService.findRuleByRuleId(ruleID.getString("ruleId"));
-        int currentStatus;
+        Rule rule = ruleService.findRuleByRuleId(ruleID.getString("deviceName"));
+        Device device = deviceRepository.findDeviceByDeviceName(ruleID.getString("ruleToDevice"));
+//        Device device = deviceService.findDeviceByDeviceId(ruleToDevice.getString("deviceId"));
+        System.out.println("TEST:"+device.getDeviceId());
+        int currentStatus = 1;
+        int deviceCurrentStatus = 1;
         if(rule.getRuleStatus() == 1){
             currentStatus = 0;
         }else {
             currentStatus = 1;
+            if(rule.getRuleExecute() == 0) {
+                deviceCurrentStatus = 0;
+            }
         }
         System.out.println("RuleController CurrentStatus:"+currentStatus);
         ruleService.changeRuleStatus(rule, currentStatus);
+        deviceService.changeDeviceStatus(device, deviceCurrentStatus);
         return currentStatus;
     }
 
