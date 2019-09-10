8c351b83f62e9e9ed570d50810bb27bf9fb04729
Cypo <voodoo.7@outlook.com>
2019-09-10 17:40:35 +0800

    debug MQ

--- src/main/java/com/jw/edge/mq/Subscriber.java
+++ src/main/java/com/jw/edge/mq/Subscriber.java
@@ -1,11 +1,23 @@
 package com.jw.edge.mq;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
 import org.springframework.stereotype.Service;
+
+import javax.jms.ConnectionFactory;
 
 
 @Service
 public class Subscriber {
+    @Bean
+    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory){
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(true);
+        return factory;
+    }
 
     @JmsListener(destination = "test.topic", containerFactory = "myJmsContainerFactory")
     public void subscribe(String text) {
