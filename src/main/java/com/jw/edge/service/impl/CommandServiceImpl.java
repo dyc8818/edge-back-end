9ef0580c73520af287e0bcc94c7be817561c7614
Cypo <voodoo.7@outlook.com>
2019-10-09 12:33:56 +0800

    command service

--- src/main/java/com/jw/edge/service/impl/CommandServiceImpl.java
+++ src/main/java/com/jw/edge/service/impl/CommandServiceImpl.java
@@ -1,15 +1,22 @@
 package com.jw.edge.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jw.edge.dao.CommandRepository;
 import com.jw.edge.entity.Command;
 import com.jw.edge.service.CommandService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CommandServiceImpl implements CommandService {
     @Autowired
     CommandRepository commandRepository;
+    @Autowired
+    RestTemplate restTemplate;
+    @Value("${server.edgex}")
+    private String ip;
 
     @Override
     public boolean addCommand(Command command){
@@ -21,4 +28,32 @@ public class CommandServiceImpl implements CommandService {
             return false;
         }
     }
+
+    @Override
+    public String deleteCommand(String thisId){
+        Command find = commandRepository.findByThisId(thisId);
+        if(find == null){
+            return "不存在该命令";
+        }else {
+            commandRepository.deleteById(thisId);
+            return "删除成功";        }
+    }
+
+    @Override
+    public JSONObject sendGet(Command command){
+        String url = "http://"+ip+":48082/api/v1/device/" + command.getDeviceId() + "/command/" + command.getCommandId();
+        try {
+            JSONObject getObj = new JSONObject(restTemplate.getForObject(url, JSONObject.class));
+            return getObj;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void sendPut(Command command, JSONObject jsonObject){
+        String url = "http://"+ip+":48082/api/v1/device/" + command.getDeviceId() + "/command/" + command.getCommandId();
+        restTemplate.put(url,jsonObject,String.class);
+    }
+
 }
