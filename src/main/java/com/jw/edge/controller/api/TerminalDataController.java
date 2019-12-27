91103e78c8069d58157a4ddb39007283f5e1bb98
KevinLord <18120131@bjtu.edu.cn>
2019-12-27 09:19:20 +0800

    rulesfinal1

--- src/main/java/com/jw/edge/controller/api/TerminalDataController.java
+++ src/main/java/com/jw/edge/controller/api/TerminalDataController.java
@@ -51,20 +51,51 @@ public class TerminalDataController {
         Terminal terminal = new Terminal();
         Terminal terminal2 = new Terminal();
         Terminal terminal3 = new Terminal();
+        Terminal terminal4 = new Terminal();
+        Terminal terminal5 = new Terminal();
+        Terminal terminal6 = new Terminal();
+        Terminal terminal7 = new Terminal();
+        Terminal terminal8 = new Terminal();
+        Terminal terminal9 = new Terminal();
+        Terminal terminal10 = new Terminal();
+
 
         kieSession.insert(terminal);
         kieSession.insert(terminal2);
         kieSession.insert(terminal3);
+        kieSession.insert(terminal4);
+        kieSession.insert(terminal5);
+        kieSession.insert(terminal6);
+        kieSession.insert(terminal7);
+        kieSession.insert(terminal8);
+        kieSession.insert(terminal9);
+        kieSession.insert(terminal10);
+
 
         kieSession.fireAllRules();//通知规则引擎执行规则
 
         int flag1=terminal.getFlag1();
         int flag2=terminal2.getFlag2();
         int flag3=terminal3.getFlag3();
+        int flag4=terminal4.getFlag4();
+        int flag5=terminal5.getFlag5();
+        int flag6=terminal6.getFlag6();
+        int flag7=terminal7.getFlag7();
+        int flag8=terminal8.getFlag8();
+        int flag9=terminal9.getFlag9();
+        int flag10=terminal10.getFlag10();
+
 
         ruleTemplet(WebDataController.threshold[0],WebDataController.parameterName[0],WebDataController.symbol[0],WebDataController.operation[0],WebDataController.service[0],flag1);
         ruleTemplet(WebDataController.threshold[1],WebDataController.parameterName[1],WebDataController.symbol[1],WebDataController.operation[1],WebDataController.service[1],flag2);
         ruleTemplet(WebDataController.threshold[2],WebDataController.parameterName[2],WebDataController.symbol[2],WebDataController.operation[2],WebDataController.service[2],flag3);
+        ruleTemplet(WebDataController.threshold[3],WebDataController.parameterName[3],WebDataController.symbol[3],WebDataController.operation[3],WebDataController.service[3],flag4);
+        ruleTemplet(WebDataController.threshold[4],WebDataController.parameterName[4],WebDataController.symbol[4],WebDataController.operation[4],WebDataController.service[4],flag5);
+        ruleTemplet(WebDataController.threshold[5],WebDataController.parameterName[5],WebDataController.symbol[5],WebDataController.operation[5],WebDataController.service[5],flag6);
+        ruleTemplet(WebDataController.threshold[6],WebDataController.parameterName[6],WebDataController.symbol[6],WebDataController.operation[6],WebDataController.service[6],flag7);
+        ruleTemplet(WebDataController.threshold[7],WebDataController.parameterName[7],WebDataController.symbol[7],WebDataController.operation[7],WebDataController.service[7],flag8);
+        ruleTemplet(WebDataController.threshold[8],WebDataController.parameterName[8],WebDataController.symbol[8],WebDataController.operation[7],WebDataController.service[8],flag9);
+        ruleTemplet(WebDataController.threshold[9],WebDataController.parameterName[9],WebDataController.symbol[9],WebDataController.operation[8],WebDataController.service[9],flag10);
 
         kieSession.dispose();
     }
--- src/main/java/com/jw/edge/entity/Terminal.java
+++ src/main/java/com/jw/edge/entity/Terminal.java
@@ -11,6 +11,13 @@ public class Terminal {
     private int flag1 = 0;
     private int flag2 = 0;
     private int flag3 = 0;
+    private int flag4 = 0;
+    private int flag5 = 0;
+    private int flag6 = 0;
+    private int flag7 = 0;
+    private int flag8 = 0;
+    private int flag9 = 0;
+    private int flag10 = 0;
 
     private int drools_threshold=WebDataController.threshold[0];
     private String drools_parameterName=WebDataController.parameterName[0];
@@ -26,6 +33,41 @@ public class Terminal {
     private String drools_parameterName3=WebDataController.parameterName[2];
     private String drools_symbol3=WebDataController.symbol[2];
     private String drools_operation3=WebDataController.operation[2];
+
+    private int drools_threshold4=WebDataController.threshold[3];
+    private String drools_parameterName4=WebDataController.parameterName[3];
+    private String drools_symbol4=WebDataController.symbol[3];
+    private String drools_operation4=WebDataController.operation[3];
+
+    private int drools_threshold5=WebDataController.threshold[4];
+    private String drools_parameterName5=WebDataController.parameterName[4];
+    private String drools_symbol5=WebDataController.symbol[4];
+    private String drools_operation5=WebDataController.operation[4];
+
+    private int drools_threshold6=WebDataController.threshold[5];
+    private String drools_parameterName6=WebDataController.parameterName[5];
+    private String drools_symbol6=WebDataController.symbol[5];
+    private String drools_operation6=WebDataController.operation[5];
+
+    private int drools_threshold7=WebDataController.threshold[6];
+    private String drools_parameterName7=WebDataController.parameterName[6];
+    private String drools_symbol7=WebDataController.symbol[6];
+    private String drools_operation7=WebDataController.operation[6];
+
+    private int drools_threshold8=WebDataController.threshold[7];
+    private String drools_parameterName8=WebDataController.parameterName[7];
+    private String drools_symbol8=WebDataController.symbol[7];
+    private String drools_operation8=WebDataController.operation[7];
+
+    private int drools_threshold9=WebDataController.threshold[8];
+    private String drools_parameterName9=WebDataController.parameterName[8];
+    private String drools_symbol9=WebDataController.symbol[8];
+    private String drools_operation9=WebDataController.operation[8];
+
+    private int drools_threshold10=WebDataController.threshold[9];
+    private String drools_parameterName10=WebDataController.parameterName[9];
+    private String drools_symbol10=WebDataController.symbol[9];
+    private String drools_operation10=WebDataController.operation[9];
 
     int terminal_value=TerminalDataController.value;
     String terminal_name=TerminalDataController.name;
@@ -44,4 +86,40 @@ public class Terminal {
         return flag3;
     }
     public int setFlag3(int num) { flag3=num; return flag3;}
+
+    public int getFlag4() {
+        return flag4;
+    }
+    public int setFlag4(int num) { flag4=num; return flag4;}
+
+    public int getFlag5() {
+        return flag5;
+    }
+    public int setFlag5(int num) { flag5=num; return flag5;}
+
+    public int getFlag6() {
+        return flag6;
+    }
+    public int setFlag6(int num) { flag6=num; return flag6;}
+
+    public int getFlag7() {
+        return flag7;
+    }
+    public int setFlag7(int num) { flag7=num; return flag7;}
+
+    public int getFlag8() {
+        return flag8;
+    }
+    public int setFlag8(int num) { flag8=num; return flag8;}
+
+    public int getFlag9() {
+        return flag9;
+    }
+    public int setFlag9(int num) { flag9=num; return flag9;}
+
+    public int getFlag10() {
+        return flag10;
+    }
+    public int setFlag10(int num) { flag10=num; return flag10;}
+
 }
